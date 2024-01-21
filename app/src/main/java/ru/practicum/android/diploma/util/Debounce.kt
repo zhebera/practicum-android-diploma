3e8fd77fffd32d47f5f1fede0b8d89e668207752
HatePK <hatepk@gmail.com>
2024-01-21 23:42:40 +0300

    Debounce class added

---
+++ app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
@@ -0,0 +1,34 @@
+package ru.practicum.android.diploma.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Debounce {
+
+    private var job: Job? = null
+
+    fun <T> debounce(
+        delayMillis: Long,
+        coroutineScope: CoroutineScope,
+        useLastParam: Boolean,
+        action: (T) -> Unit
+    ): (T) -> Unit {
+        return { param: T ->
+            if (useLastParam) {
+                job?.cancel()
+            }
+            if (job?.isCompleted != false || useLastParam) {
+                job = coroutineScope.launch {
+                    delay(delayMillis)
+                    action(param)
+                }
+            }
+        }
+    }
+
+    fun cancel() {
+        job?.cancel()
+    }
+}
