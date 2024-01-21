2c61005a658fd0a32ea17dc4b554cb1acb40bdea
void-85 <zGonzo@doctor.com>
2024-01-22 00:33:31 +0500

    fix #6 (2) Настройка build.gradle-файлов (minSdk == 26 в файле /gradle/libs.versions.toml)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,20 +61,14 @@ dependencies {
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
 
-
-
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-
-    implementation     ("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
-
     implementation("io.insert-koin:koin-android:3.5.0")
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
