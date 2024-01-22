004f2cbc38d94e614d5a5b632c8ab12ac2a758f3
HatePK <hatepk@gmail.com>
2024-01-22 14:35:17 +0300

    Fix #5 задачи
    glide and dpToPx funs added

---
+++ app/src/main/java/ru/practicum/android/diploma/util/utilFuns.kt
@@ -0,0 +1,28 @@
+package ru.practicum.android.diploma.util
+
+import android.content.Context
+import android.util.TypedValue
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+
+fun glide(context: Context,
+          source: String,
+          view: ImageView,
+          transformation: BitmapTransformation
+) {
+
+    Glide.with(context)
+        .load(source)
+//        .placeholder() - добавить, когда появится res файл
+        .transform(transformation)
+        .into(view)
+
+}
+
+fun dpToPx(dp: Float, context: Context): Int {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        context.resources.displayMetrics).toInt()
+}
