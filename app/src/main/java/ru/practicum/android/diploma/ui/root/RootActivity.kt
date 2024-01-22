ae6e13fa371ccb15dc8c149d79e3255855a3988b
Denis Brusov <85849855+Jahnta@users.noreply.github.com>
2024-01-22 21:24:59 +0300

    Update RootActivity.kt
    Fix #9 
    Отменены изменения RootActivity

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,25 +2,20 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityRootBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRootBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setupWithNavController(navController)
+        // Пример использования access token для HeadHunter API
+        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
     }
+
+    private fun networkRequestExample(accessToken: String) {
+        // ...
+    }
+
 }
