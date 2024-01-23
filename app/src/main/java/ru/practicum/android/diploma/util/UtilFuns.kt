8be50ae7571a897e32fad7f747dac79424b0a10b
HatePK <hatepk@gmail.com>
2024-01-23 12:40:23 +0300

    Fix #17 задачи - DI
    Исправлены замечания detektor

--- app/src/main/java/ru/practicum/android/diploma/util/UtilFuns.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/UtilFuns.kt
@@ -34,10 +34,12 @@ fun dpToPx(dp: Float, context: Context): Int {
         .toInt()
 }
 
-fun <T> debounce(delayMillis: Long,
-                 coroutineScope: CoroutineScope,
-                 useLastParam: Boolean,
-                 action: (T) -> Unit): (T) -> Unit {
+fun <T> debounce(
+    delayMillis: Long,
+     coroutineScope: CoroutineScope,
+     useLastParam: Boolean,
+     action: (T) -> Unit
+): (T) -> Unit {
     var debounceJob: Job? = null
     return { param: T ->
         if (useLastParam) {
