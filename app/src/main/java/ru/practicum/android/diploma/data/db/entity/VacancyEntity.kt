b54476a0c95bf9ddacd86193f0d95bd254c4cdc9
zhebera <zhebera@yandex.by>
2024-01-23 10:11:10 +0300

    fix #13 fix detekt errors_2

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "vacancy_table")
 data class VacancyEntity(
+    @PrimaryKey
     val id: String,
     val name: String
 )
