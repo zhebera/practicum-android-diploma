03701f81f50f461d8fd56de21c66288b52081576
void-85 <zGonzo@doctor.com>
2024-01-23 21:29:25 +0500

    fix #10 : перенесены в готовые заглушки

--- app/src/main/java/ru/practicum/android/diploma/ui/team/fragment/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/fragment/TeamFragment.kt
@@ -13,15 +13,14 @@ class TeamFragment : Fragment() {
     private var _binding: FragmentTeamBinding? = null
     private val binding get() = _binding!!
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTeamBinding.inflate(inflater, container, false)
         return binding.root
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
