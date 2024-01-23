0ee4b57f7642e7c5f1300512dc0d4db01a68cc1e
HatePK <hatepk@gmail.com>
2024-01-23 12:36:23 +0300

    Fix #17 задачи - DI
    Исправлены замечания

--- app/src/main/java/ru/practicum/android/diploma/util/UtilFuns.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/UtilFuns.kt
@@ -5,16 +5,21 @@ import android.util.TypedValue
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.R
 
 fun glide(
     context: Context,
     source: String,
     view: ImageView,
-    transformation: BitmapTransformation
+    transformation: BitmapTransformation? = null
 ) {
     Glide.with(context)
         .load(source)
-//        .placeholder() - добавить, когда появится res файл
+        .placeholder(R.drawable.placeholder)
         .transform(transformation)
         .into(view)
 
@@ -27,4 +32,22 @@ fun dpToPx(dp: Float, context: Context): Int {
         context.resources.displayMetrics
     )
         .toInt()
+}
+
+fun <T> debounce(delayMillis: Long,
+                 coroutineScope: CoroutineScope,
+                 useLastParam: Boolean,
+                 action: (T) -> Unit): (T) -> Unit {
+    var debounceJob: Job? = null
+    return { param: T ->
+        if (useLastParam) {
+            debounceJob?.cancel()
+        }
+        if (debounceJob?.isCompleted != false || useLastParam) {
+            debounceJob = coroutineScope.launch {
+                delay(delayMillis)
+                action(param)
+            }
+        }
+    }
 }
