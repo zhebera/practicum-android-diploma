e88d92815fe6b31936322b02e3c80bc3f7d49f87
zhebera <zhebera@yandex.by>
2024-01-23 09:50:13 +0300

    fix #13 add database builder  in DI

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,14 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.db.AppDatabase
 
-val dataModule = module {}
+val dataModule = module {
+
+    single<AppDatabase>{
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .build()
+    }
+}
