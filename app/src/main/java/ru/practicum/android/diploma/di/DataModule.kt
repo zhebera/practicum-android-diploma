f325eb4791958f777cb6a0c15abc9608bdfed14d
zhebera <zhebera@yandex.by>
2024-01-25 17:55:20 +0300

    close #37 add changes in di for created classes/ dleete gitkepp files

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,15 +1,17 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.db.AppDatabase
 import ru.practicum.android.diploma.data.network.HeadHunterApi
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.util.API_URL
-import ru.practicum.android.diploma.data.db.AppDatabase
-import androidx.room.Room
-import org.koin.android.ext.koin.androidContext
 
 val dataModule = module {
 
@@ -23,8 +25,10 @@ val dataModule = module {
     }
 
     single<NetworkClient> {
-        RetrofitNetworkClient(get())
+        RetrofitNetworkClient(get(), androidContext())
     }
+
+    factory { Gson() }
 
     single<AppDatabase> {
         Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,5 +1,15 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.converters.VacanciesConverter
+import ru.practicum.android.diploma.data.search.SearchRepositoryImpl
+import ru.practicum.android.diploma.domain.api.search.SearchRepository
 
-val repositoryModule = module {}
+val repositoryModule = module {
+
+    factory { VacanciesConverter() }
+
+    single<SearchRepository>{
+        SearchRepositoryImpl(get(), get())
+    }
+}
