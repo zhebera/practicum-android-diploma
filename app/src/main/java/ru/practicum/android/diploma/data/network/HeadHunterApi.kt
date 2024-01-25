3696728fb262919ac5eae4b32f24a1dc78927252
zhebera <zhebera@yandex.by>
2024-01-25 17:45:30 +0300

    close #37 add suspend fun searching in HeadHunterApi

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
@@ -1,3 +1,13 @@
 package ru.practicum.android.diploma.data.network
 
-interface HeadHunterApi
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.practicum.android.diploma.data.response.VacanciesResponse
+
+interface HeadHunterApi {
+
+    @GET("/vacancies")
+    suspend fun getVacancies(
+        @Query("text") vacancy: String
+    ): VacanciesResponse
+}
