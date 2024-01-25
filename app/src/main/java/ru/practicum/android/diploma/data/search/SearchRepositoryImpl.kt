6f1b4d1365e8ffaf1879c9bf6ac2601230ed9076
zhebera <zhebera@yandex.by>
2024-01-25 17:52:12 +0300

    close #37 add implementation of SearchRepository and SearchInteractor

---
+++ app/src/main/java/ru/practicum/android/diploma/data/search/SearchRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package ru.practicum.android.diploma.data.search
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.data.converters.VacanciesConverter
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.request.SearchRequest
+import ru.practicum.android.diploma.data.response.VacanciesResponse
+import ru.practicum.android.diploma.domain.api.search.SearchRepository
+import ru.practicum.android.diploma.domain.models.Vacancies
+import ru.practicum.android.diploma.util.Resource
+
+class SearchRepositoryImpl(
+    private val networkClient: NetworkClient,
+    private val vacanciesConverter: VacanciesConverter
+) : SearchRepository {
+    override fun searchVacancies(vacancy: String): Flow<Resource<Vacancies>> = flow {
+        val response = networkClient.doRequest(SearchRequest(vacancy))
+        when (response.resultCode) {
+            -1 -> {
+                emit(Resource.Error("Проверьте подключение к интернету"))
+            }
+            200 -> {
+                with(response as VacanciesResponse){
+                    val data = vacanciesConverter.convert(response)
+                    emit(Resource.Success(data))
+                }
+            }
+            else -> emit(Resource.Error("Ошибка сервера"))
+        }
+    }
+}
