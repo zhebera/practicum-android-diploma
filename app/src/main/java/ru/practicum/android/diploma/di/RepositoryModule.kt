58761ff6c3fb52206544887a214f96cde34fb758
zhebera <zhebera@yandex.by>
2024-01-26 16:53:17 +0300

    fix #37 change di for SearchRepository

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.converters.VacanciesConverter
 import ru.practicum.android.diploma.data.search.SearchRepositoryImpl
@@ -10,6 +11,6 @@ val repositoryModule = module {
     factory { VacanciesConverter() }
 
     single<SearchRepository> {
-        SearchRepositoryImpl(get(), get())
+        SearchRepositoryImpl(get(), get(), androidContext())
     }
 }
