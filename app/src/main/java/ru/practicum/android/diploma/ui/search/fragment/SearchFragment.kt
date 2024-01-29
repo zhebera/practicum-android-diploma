0276033d1cd4dd596b5917e69ea770375e31f905
Ksenia <averkieva10012017@yandex.ru>
2024-01-30 00:04:30 +0300

    Fix#38

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/SearchFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
@@ -40,7 +41,7 @@ class SearchFragment : Fragment() {
     private val searchViewModel by viewModel<SearchViewModel>()
     private var textWatcher: TextWatcher? = null
     private var savedSearchEditText: String? = null
-
+    private lateinit var bottomNavigator: BottomNavigationView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -53,6 +54,8 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvTitle.text = getString(R.string.main)
+
+        bottomNavigator = requireActivity().findViewById(R.id.bottomNavigationView)
 
         binding.ivSearchImage.setOnClickListener {
             if (binding.etSearch.toString().isNotEmpty()) {
@@ -148,6 +151,7 @@ class SearchFragment : Fragment() {
             llProblem.visibility = View.GONE
             pbCentralProgressBar.visibility = View.GONE
             vacancyAdapter.setData(vacancies)
+            closeKeyboard()
         }
     }
 
