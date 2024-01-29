8d018bbf7c2958a7fdd3030844a5311149be2f83
zhebera <zhebera@yandex.by>
2024-01-29 16:55:41 +0300

    close #35 add db interfaces in domain layer

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.data.db.dao.VacancyDao
-import ru.practicum.android.diploma.data.db.entity.VacancyEntity
+import ru.practicum.android.diploma.data.db.entity.VacancyDescriptionEntity
 
-@Database(entities = [VacancyEntity::class], version = 1)
+@Database(entities = [VacancyDescriptionEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun vacancyDao(): VacancyDao
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/favourite/FavouriteRepository.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.domain.api.favourite
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.VacancyDescription
+
+interface FavouriteRepository {
+
+    fun getAllVacancies(): Flow<List<VacancyDescription>>
+    fun addVacancy(vacancy: VacancyDescription)
+    fun removeVacancy(vacancy: VacancyDescription)
+}
