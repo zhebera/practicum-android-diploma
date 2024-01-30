7755f6f061896427297ad6e2d14a39ce6e3ee224
zhebera <zhebera@yandex.by>
2024-01-30 17:34:22 +0300

    close #36 add adapter for FavouriteFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/favourite/fragment/FavouriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favourite/fragment/FavouriteFragment.kt
@@ -5,26 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavouriteBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.ui.search.adapter.VacancyAdapter
+import ru.practicum.android.diploma.util.CLICK_DEBOUNCE_DELAY
+import ru.practicum.android.diploma.util.VACANCY
+import ru.practicum.android.diploma.util.debounce
 
 class FavouriteFragment : Fragment() {
 
     private var _binding: FragmentFavouriteBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var onVacancyClickDebounce: (Vacancy) -> Unit
+    private val adapter = VacancyAdapter{
+        onVacancyClickDebounce(it)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.text = getString(R.string.favourite)
+
+        onVacancyClickDebounce = debounce(CLICK_DEBOUNCE_DELAY, viewLifecycleOwner.lifecycleScope, true) { vacancy ->
+            val bundle = Bundle()
+            bundle.putParcelable(VACANCY, vacancy)
+            findNavController().navigate(R.id.action_favouriteFragment_to_detailsFragment, bundle)
+        }
+
+        binding.rvFavourite.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvFavourite.adapter = adapter
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/favourite/viewmodel/FavouriteviewModel.kt
@@ -0,0 +1,2 @@
+package ru.practicum.android.diploma.ui.favourite.viewmodel
+
