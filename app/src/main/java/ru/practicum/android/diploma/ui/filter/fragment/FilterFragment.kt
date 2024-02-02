1250d4d84d9ece3800cdbe79cbae237edf941b05
Ksenia <averkieva10012017@yandex.ru>
2024-02-02 10:41:53 +0300

    Fix#65

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFilterBinding
 
 class FilterFragment : Fragment() {
 
