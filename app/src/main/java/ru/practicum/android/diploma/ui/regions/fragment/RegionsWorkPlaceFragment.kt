919cb9b10ae6c850f8a396379b9ec7ef88ea15f2
Jahnta <dabrusov@gmail.com>
2024-02-04 02:50:31 +0300

    Fix #94 Добавлена логика перехода назад с экрана "Регионы" на экран "Место работы"

--- app/src/main/java/ru/practicum/android/diploma/ui/regions/fragment/RegionsWorkPlaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/regions/fragment/RegionsWorkPlaceFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentFilterRegionBinding
 
 class RegionsWorkPlaceFragment : Fragment() {
@@ -16,6 +18,20 @@ class RegionsWorkPlaceFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentFilterRegionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        })
     }
 
     override fun onDestroyView() {
