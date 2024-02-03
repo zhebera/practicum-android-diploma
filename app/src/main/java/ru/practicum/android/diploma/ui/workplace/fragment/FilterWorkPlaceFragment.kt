0de5f206ef04e6c91c62579f11e033d5b25668b6
zhebera <zhebera@yandex.by>
2024-02-04 00:01:27 +0300

    close #86 реализация перехода назад на экран фильтрации с экрана выбора места

--- app/src/main/java/ru/practicum/android/diploma/ui/workplace/fragment/FilterWorkPlaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/workplace/fragment/FilterWorkPlaceFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentFilterPlaceOfWorkBinding
 
 class FilterWorkPlaceFragment : Fragment() {
@@ -16,6 +18,20 @@ class FilterWorkPlaceFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFilterPlaceOfWorkBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivFilterPlaceOfWorkBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        })
     }
 
     override fun onDestroy() {
