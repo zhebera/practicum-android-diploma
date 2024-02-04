ba77b3781aeeb71bc08112ddc40de2aff4b201c0
zhebera <zhebera@yandex.by>
2024-02-04 11:36:45 +0300

    close #97 add countries di repository and interactor

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,11 +1,13 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.api.countries.CountriesInteractor
 import ru.practicum.android.diploma.domain.api.favourite.FavouriteInteractor
 import ru.practicum.android.diploma.domain.api.search.SearchInteractor
 import ru.practicum.android.diploma.domain.api.details.DetailsInteractor
 import ru.practicum.android.diploma.domain.impl.FavouriteInteractorImpl
 import ru.practicum.android.diploma.domain.impl.SearchInteractorImpl
+import ru.practicum.android.diploma.domain.impl.countries.CountriesInteractorImpl
 import ru.practicum.android.diploma.domain.impl.details.DetailsInteractorImpl
 
 val interactorModule = module {
@@ -20,5 +22,9 @@ val interactorModule = module {
 
     single<FavouriteInteractor> {
         FavouriteInteractorImpl(get())
+    }
+
+    single<CountriesInteractor>{
+        CountriesInteractorImpl(get())
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -5,9 +5,11 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.data.converters.VacanciesConverter
 import ru.practicum.android.diploma.data.converters.VacanciesDescriptionConverter
 import ru.practicum.android.diploma.data.converters.VacancyDbConverter
+import ru.practicum.android.diploma.data.countries.CountriesRepositoryImpl
 import ru.practicum.android.diploma.data.favourite.FavouriteRepositoryImpl
 import ru.practicum.android.diploma.data.search.SearchRepositoryImpl
 import ru.practicum.android.diploma.data.details.DetailsRepositoryImpl
+import ru.practicum.android.diploma.domain.api.countries.CountriesRepository
 import ru.practicum.android.diploma.domain.api.favourite.FavouriteRepository
 import ru.practicum.android.diploma.domain.api.search.SearchRepository
 import ru.practicum.android.diploma.domain.api.details.DetailsRepository
@@ -35,5 +37,9 @@ val repositoryModule = module {
 
     single<FavouriteRepository> {
         FavouriteRepositoryImpl(get(), get())
+    }
+
+    single<CountriesRepository>{
+        CountriesRepositoryImpl()
     }
 }
