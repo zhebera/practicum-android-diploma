881fe7e1ab82dadc15ca3668fab0f9b20b7aad00
HatePK <hatepk@gmail.com>
2024-02-04 21:18:41 +0300

    Fix #105 - detekt

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/regions/RegionsInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/regions/RegionsInteractorImpl.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.domain.impl.regions
 import ru.practicum.android.diploma.domain.api.regions.RegionsInteractor
 import ru.practicum.android.diploma.domain.api.regions.RegionsRepository
 
-class RegionsInteractorImpl(private val regionsRepository: RegionsRepository): RegionsInteractor {
+class RegionsInteractorImpl(private val regionsRepository: RegionsRepository) : RegionsInteractor {
 
     override fun getRegions(areaId: String?) {
         regionsRepository.getRegions(areaId)
