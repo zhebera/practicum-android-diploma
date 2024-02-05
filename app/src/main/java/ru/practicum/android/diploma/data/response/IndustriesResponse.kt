89a5705af01385292af0fb1729beadca8a3a86c2
Ksenia <averkieva10012017@yandex.ru>
2024-02-05 15:46:31 +0300

    Fix#102

--- app/src/main/java/ru/practicum/android/diploma/data/response/IndustriesResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/response/IndustriesResponse.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.data.response
 
 import ru.practicum.android.diploma.data.dto.Response
+
 data class IndustryResponse(
     val id: String,
     val name: String
