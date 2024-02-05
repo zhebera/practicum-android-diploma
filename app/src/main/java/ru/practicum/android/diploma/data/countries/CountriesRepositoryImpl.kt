23e4d83e9eaa9d1a0e2b35b80f4b87e2b01e45ae
zhebera <zhebera@yandex.by>
2024-02-05 11:31:48 +0300

    close #100 add CountriesRepositoryImpl and change InteractorImpl

--- app/src/main/java/ru/practicum/android/diploma/data/countries/CountriesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/countries/CountriesRepositoryImpl.kt
@@ -1,11 +1,47 @@
 package ru.practicum.android.diploma.data.countries
 
+import android.content.Context
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.data.converters.CountriesConverter
+import ru.practicum.android.diploma.data.converters.VacanciesConverter
+import ru.practicum.android.diploma.data.dto.ResponseCode
+import ru.practicum.android.diploma.data.dto.VacancyDescriptionRequest
+import ru.practicum.android.diploma.data.dto.VacancyDescriptionResponse
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.request.CountriesRequest
+import ru.practicum.android.diploma.data.response.CountriesResponse
 import ru.practicum.android.diploma.domain.api.countries.CountriesRepository
 import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.util.Resource
 
-class CountriesRepositoryImpl : CountriesRepository {
-    override fun getCountries(): Flow<List<Country>> {
-        TODO("Not yet implemented")
+class CountriesRepositoryImpl(
+    private val networkClient: NetworkClient,
+    private val countriesConverter: CountriesConverter,
+    context: Context
+) : CountriesRepository {
+    private val badConnection by lazy {
+        context.getString(R.string.bad_connection)
+    }
+    private val serverError by lazy {
+        context.getString(R.string.server_error)
+    }
+    override fun getCountries(): Flow<Resource<List<Country>>> = flow{
+        val response = networkClient.doRequest(CountriesRequest())
+        when (response.resultCode) {
+            ResponseCode.NETWORK_FAILED -> {
+                emit(Resource.Error(message = badConnection))
+            }
+
+            ResponseCode.SUCCESS -> {
+                with(response as CountriesResponse) {
+                    val data = countriesConverter.map(response)
+                    emit(Resource.Success(data))
+                }
+            }
+
+            else -> emit(Resource.Error(message = serverError))
+        }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/api/countries/CountriesInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/countries/CountriesInteractor.kt
@@ -2,8 +2,9 @@ package ru.practicum.android.diploma.domain.api.countries
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.util.Resource
 
 interface CountriesInteractor {
 
-    fun getCountries(): Flow<List<Country>>
+    fun getCountries(): Flow<Pair<List<Country>?, String?>>
 }
