46d3140d985762e540c1b9ffd12a4c952652e53c
Ksenia <averkieva10012017@yandex.ru>
2024-02-05 13:00:27 +0300

    Fix#99

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -5,16 +5,12 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.data.converters.VacanciesConverter
 import ru.practicum.android.diploma.data.converters.VacanciesDescriptionConverter
 import ru.practicum.android.diploma.data.converters.VacancyDbConverter
-import ru.practicum.android.diploma.data.countries.CountriesRepositoryImpl
 import ru.practicum.android.diploma.data.details.DetailsRepositoryImpl
 import ru.practicum.android.diploma.data.favourite.FavouriteRepositoryImpl
 import ru.practicum.android.diploma.data.search.SearchRepositoryImpl
-import ru.practicum.android.diploma.domain.api.countries.CountriesRepository
 import ru.practicum.android.diploma.domain.api.details.DetailsRepository
-import ru.practicum.android.diploma.data.industries.IndustriesRepositoryImpl
 import ru.practicum.android.diploma.domain.api.favourite.FavouriteRepository
 import ru.practicum.android.diploma.domain.api.search.SearchRepository
-import ru.practicum.android.diploma.domain.api.industries.IndustriesRepository
 
 val repositoryModule = module {
 
