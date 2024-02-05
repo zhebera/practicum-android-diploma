f4083e761ad52cb43d5265ded0485d1c106efad7
zhebera <zhebera@yandex.by>
2024-02-05 16:38:59 +0300

    close #100 merge conflicts

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -6,9 +6,11 @@ import ru.practicum.android.diploma.data.converters.CountriesConverter
 import ru.practicum.android.diploma.data.converters.VacanciesConverter
 import ru.practicum.android.diploma.data.converters.VacanciesDescriptionConverter
 import ru.practicum.android.diploma.data.converters.VacancyDbConverter
+import ru.practicum.android.diploma.data.countries.CountriesRepositoryImpl
 import ru.practicum.android.diploma.data.details.DetailsRepositoryImpl
 import ru.practicum.android.diploma.data.favourite.FavouriteRepositoryImpl
 import ru.practicum.android.diploma.data.search.SearchRepositoryImpl
+import ru.practicum.android.diploma.domain.api.countries.CountriesRepository
 import ru.practicum.android.diploma.domain.api.details.DetailsRepository
 import ru.practicum.android.diploma.domain.api.favourite.FavouriteRepository
 import ru.practicum.android.diploma.domain.api.search.SearchRepository
