6a7b9c5568bf2c0c8ca901fb1b3bc183d2c7b161
Jahnta <dabrusov@gmail.com>
2024-02-06 01:14:54 +0300

    Fix #117 Правки detekt1

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
@@ -16,7 +16,9 @@ class FilterFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFilterBinding.inflate(inflater, container, false)
         return binding.root
