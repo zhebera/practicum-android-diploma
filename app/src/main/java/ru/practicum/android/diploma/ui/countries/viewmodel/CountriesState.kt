3e540df0e69d6c846d05a974677df578fb562988
zhebera <zhebera@yandex.by>
2024-02-05 16:59:11 +0300

    close #110 add CountriesState

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/countries/viewmodel/CountriesState.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.ui.countries.viewmodel
+
+import ru.practicum.android.diploma.domain.models.Country
+
+sealed interface CountriesState {
+
+    object Loading : CountriesState
+
+    object Empty : CountriesState
+
+    data class Content(
+        val data: List<Country>
+    ) : CountriesState
+
+    data class Error(
+        val message: String
+    ) : CountriesState
+}
