2f7e09608c205399c4d6148823224b3a257068b9
HatePK <hatepk@gmail.com>
2024-02-06 23:08:04 +0300

    Fix #123 - fix

--- app/src/main/java/ru/practicum/android/diploma/data/sharedpreferences/SharedPreferencesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/sharedpreferences/SharedPreferencesRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.data.sharedpreferences
 
-import com.google.gson.Gson
-import android.content.Context
 import ru.practicum.android.diploma.domain.api.sharedpreferences.SharedPreferencesRepository
 import ru.practicum.android.diploma.domain.models.FilterModel
 import ru.practicum.android.diploma.util.FILTER_OBJECT_KEY
 import ru.practicum.android.diploma.util.CAREER_KEY_APP_PREFERENCES
+import com.google.gson.Gson
+import android.content.Context
 
 class SharedPreferencesRepositoryImpl(
     context: Context
