c8783bc65c7058f9d62cc42cd0a623adc1791731
HatePK <hatepk@gmail.com>
2024-02-07 15:01:38 +0300

    Fix #137

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/fragment/FilterFragment.kt
@@ -5,15 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
+import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.domain.models.Industry
+import ru.practicum.android.diploma.domain.models.Region
+import ru.practicum.android.diploma.util.COUNTRY_BACKSTACK_KEY
+import ru.practicum.android.diploma.util.INDUSTRY_BACKSTACK_KEY
+import ru.practicum.android.diploma.util.REGION_BACKSTACK_KEY
 
 class FilterFragment : Fragment() {
 
     private var _binding: FragmentFilterBinding? = null
     private val binding get() = _binding!!
+
+    private var country: Country? = null
+    private var region: Region? = null
+    private var industry: Industry? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,8 +37,51 @@ class FilterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvFilterMainText.text = getString(R.string.setting_of_filter)
 
+        setBackStackListeners()
+        setButtonsListeners()
+    }
+
+    private fun setBackStackListeners() {
+        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Industry>(INDUSTRY_BACKSTACK_KEY)?.observe(
+            viewLifecycleOwner
+        ) { backStackIndustry ->
+            if (backStackIndustry != null) {
+                industry = backStackIndustry
+
+                binding.etIndustry.setText(industry?.name)
+                binding.tvApply.isVisible = true
+                binding.tvRemove.isVisible = true
+            }
+        }
+
+        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Country>(COUNTRY_BACKSTACK_KEY)?.observe(
+            viewLifecycleOwner
+        ) { backStackCountry ->
+            if (backStackCountry != null) {
+                country = backStackCountry
+
+                binding.etIndustry.setText(country?.name)
+                binding.tvApply.isVisible = true
+                binding.tvRemove.isVisible = true
+            }
+        }
+
+        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Region>(REGION_BACKSTACK_KEY)?.observe(
+            viewLifecycleOwner
+        ) { backStackRegion ->
+            if (backStackRegion != null) {
+                region = backStackRegion
+
+                val fullWorkPlace = "${country?.name}, ${region?.name}"
+                binding.etIndustry.setText(fullWorkPlace)
+                binding.tvApply.isVisible = true
+                binding.tvRemove.isVisible = true
+            }
+        }
+    }
+
+    private fun setButtonsListeners() {
         binding.etPlaceOfWork.setOnClickListener {
             findNavController().navigate(R.id.action_filterFragment_to_filterWorkPlaceFragment)
         }
--- app/src/main/java/ru/practicum/android/diploma/util/Consts.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Consts.kt
@@ -5,3 +5,6 @@ const val VACANCY = "vacancy"
 const val CLICK_DEBOUNCE_DELAY = 2000L
 const val CAREER_KEY_APP_PREFERENCES = "career_key_app_preferences"
 const val FILTER_OBJECT_KEY = "filter_object"
+const val INDUSTRY_BACKSTACK_KEY = "industries_key"
+const val COUNTRY_BACKSTACK_KEY = "country_key"
+const val REGION_BACKSTACK_KEY = "region_key"
