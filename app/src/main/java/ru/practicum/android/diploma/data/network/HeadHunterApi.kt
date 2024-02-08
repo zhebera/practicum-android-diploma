7a7da5e8e48868cc2782ac417c7ed2c2a361ae83
Jahnta <dabrusov@gmail.com>
2024-02-08 22:22:36 +0300

    Fix #167 First commit

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
@@ -1,9 +1,10 @@
 package ru.practicum.android.diploma.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
-import retrofit2.http.Query
 import retrofit2.http.QueryMap
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.data.dto.IndustriesDto
 import ru.practicum.android.diploma.data.dto.VacancyDescriptionResponse
 import ru.practicum.android.diploma.data.response.CountryResponse
@@ -11,12 +12,19 @@ import ru.practicum.android.diploma.data.response.RegionResponse
 import ru.practicum.android.diploma.data.response.VacanciesResponse
 
 interface HeadHunterApi {
-
+    @Headers(
+        "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}",
+        "HH-User-Agent: Career Key (averkieva10012017@yandex.ru)"
+    )
     @GET("/vacancies")
     suspend fun getVacancies(
         @QueryMap options: Map<String, String>
     ): VacanciesResponse
 
+    @Headers(
+        "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}",
+        "HH-User-Agent: Career Key (averkieva10012017@yandex.ru)"
+    )
     @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancyDescription(@Path("vacancy_id") vacancyId: String): VacancyDescriptionResponse
 
