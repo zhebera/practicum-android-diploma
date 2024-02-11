e6f31b16b44e1c6b5442e6bed21ab1c1c004c407
HatePK <hatepk@gmail.com>
2024-02-11 22:08:44 +0300

    Fix #146

--- app/src/main/java/ru/practicum/android/diploma/ui/workplace/fragment/FilterWorkPlaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/workplace/fragment/FilterWorkPlaceFragment.kt
@@ -1,12 +1,18 @@
 package ru.practicum.android.diploma.ui.workplace.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterPlaceOfWorkBinding
 import ru.practicum.android.diploma.domain.models.Country
@@ -23,6 +29,13 @@ class FilterWorkPlaceFragment : Fragment() {
     private var regionModel: Region? = null
     private var countryModel: Country? = null
 
+    private var countryContainer: TextInputLayout? = null
+    private var countryTextInput: TextInputEditText? = null
+    private var regionTextInput: TextInputEditText? = null
+    private var regionContainer: TextInputLayout? = null
+    private var submitButton: TextView? = null
+    private var backButton: ImageView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFilterPlaceOfWorkBinding.inflate(inflater, container, false)
         return binding.root
@@ -31,27 +44,29 @@ class FilterWorkPlaceFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(findNavController().currentBackStackEntry?.savedStateHandle) {
-            this?.getLiveData<Country>(COUNTRY_BACKSTACK_KEY)?.observe(viewLifecycleOwner) { country ->
-                countryModel = country
-                if (country != null) {
-                    binding.etCountry.setText(country.name)
-                    binding.tvSelect.visibility = View.VISIBLE
-                }
-            }
+        setViews()
+        setOnClickListeners()
+        setTextChangedListeners()
+        setBackStackListeners()
+    }
 
-            this?.getLiveData<Region>(REGION_BACKSTACK_KEY)?.observe(viewLifecycleOwner) { region ->
-                regionModel = region
-                countryModel = region.parentCountry
-                if (region != null) {
-                    binding.etRegion.setText(region.name)
-                    binding.etCountry.setText(region.parentCountry?.name)
-                    binding.tvSelect.visibility = View.VISIBLE
-                }
-            }
+    private fun setViews() {
+        countryContainer = binding.country
+        countryTextInput = binding.etCountry
+        regionTextInput = binding.etRegion
+        regionContainer = binding.region
+        submitButton = binding.tvSelect
+        backButton = binding.ivFilterPlaceOfWorkBackButton
+    }
+
+    private fun setOnClickListeners() {
+        countryContainer?.setEndIconOnClickListener {
+            findNavController().navigate(
+                R.id.action_filterWorkPlaceFragment_to_countriesWorkPlaceFragment
+            )
         }
 
-        binding.etRegion.setOnClickListener {
+        regionContainer?.setEndIconOnClickListener {
             val countryId = if (countryModel != null) {
                 countryModel!!.id
             } else {
@@ -63,17 +78,13 @@ class FilterWorkPlaceFragment : Fragment() {
             )
         }
 
-        binding.etCountry.setOnClickListener {
-            findNavController().navigate(R.id.action_filterWorkPlaceFragment_to_countriesWorkPlaceFragment)
-        }
-
-        binding.tvSelect.setOnClickListener {
+        submitButton?.setOnClickListener {
             findNavController().previousBackStackEntry?.savedStateHandle?.set(COUNTRY_BACKSTACK_KEY, countryModel)
             findNavController().previousBackStackEntry?.savedStateHandle?.set(REGION_BACKSTACK_KEY, regionModel)
             findNavController().popBackStack()
         }
 
-        binding.ivFilterPlaceOfWorkBackButton.setOnClickListener {
+        backButton?.setOnClickListener {
             findNavController().popBackStack()
         }
 
@@ -82,6 +93,94 @@ class FilterWorkPlaceFragment : Fragment() {
                 findNavController().popBackStack()
             }
         })
+    }
+
+    private fun setTextChangedListeners() {
+        val countryTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+            override fun afterTextChanged(s: Editable?) {
+                with(countryContainer!!) {
+                    if (s.isNullOrBlank()) {
+                        setEndIconDrawable(R.drawable.arrow_forward)
+                        setEndIconOnClickListener {
+                            findNavController().navigate(
+                                R.id.action_filterWorkPlaceFragment_to_countriesWorkPlaceFragment
+                            )
+                        }
+                    } else {
+                        endIconMode = TextInputLayout.END_ICON_CUSTOM
+                        setEndIconDrawable(R.drawable.close)
+
+                        setEndIconOnClickListener {
+                            s.clear()
+                            regionTextInput?.text?.clear()
+
+                            countryModel = null
+                            regionModel = null
+                        }
+                    }
+                }
+            }
+        }
+
+        val regionTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+            override fun afterTextChanged(s: Editable?) {
+                with(regionContainer!!) {
+                    if (s.isNullOrBlank()) {
+                        setEndIconDrawable(R.drawable.arrow_forward)
+                        setEndIconOnClickListener {
+                            val countryId = if (countryModel != null) {
+                                countryModel!!.id
+                            } else {
+                                ""
+                            }
+                            findNavController().navigate(
+                                R.id.action_filterWorkPlaceFragment_to_regionsWorkPlaceFragment,
+                                RegionsWorkPlaceFragment.createArgs(countryId)
+                            )
+                        }
+                    } else {
+                        endIconMode = TextInputLayout.END_ICON_CUSTOM
+                        setEndIconDrawable(R.drawable.close)
+
+                        setEndIconOnClickListener {
+                            s.clear()
+                            regionModel = null
+                        }
+                    }
+                }
+            }
+        }
+
+        countryTextInput?.addTextChangedListener(countryTextWatcher)
+        regionTextInput?.addTextChangedListener(regionTextWatcher)
+    }
+
+    private fun setBackStackListeners() {
+        with(findNavController().currentBackStackEntry?.savedStateHandle) {
+            this?.getLiveData<Country>(COUNTRY_BACKSTACK_KEY)?.observe(viewLifecycleOwner) { country ->
+                countryModel = country
+                if (country != null) {
+                    countryTextInput?.setText(country.name)
+                    submitButton?.visibility = View.VISIBLE
+                }
+            }
+
+            this?.getLiveData<Region>(REGION_BACKSTACK_KEY)?.observe(viewLifecycleOwner) { region ->
+                regionModel = region
+                countryModel = region.parentCountry
+                if (region != null) {
+                    regionTextInput?.setText(region.name)
+                    countryTextInput?.setText(region.parentCountry?.name)
+                    submitButton?.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
